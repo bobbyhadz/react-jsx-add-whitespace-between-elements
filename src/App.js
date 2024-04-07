202931418fe94f61c98ab09096aaf1bd75d67984
bobbyhadz <opmftw@gmail.com>
2024-04-07 16:30:04 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{margin: 50}}>
+      <h2>
+        <span style={{textDecoration: 'line-through'}}>
+          bobby
+        </span>{' '}
+        hadz
+      </h2>
+    </div>
+  );
+}
+
+export default App;
